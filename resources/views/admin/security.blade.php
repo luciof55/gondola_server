b7f24f1bade50ea2178e60424f965ab78e002a88
Lucio Ferrero <lferrero@topgroup.com.ar>
2018-06-13 15:49:08 -0300

    Add table column order

--- resources/views/admin/security.blade.php
+++ resources/views/admin/security.blade.php
@@ -9,8 +9,12 @@
 			<div class="d-flex flex-column">
 				<div class="p-2 text-white bg-info border-bottom rounded-top">Security</div>
 			</div>
-			<div class="row">
-				<div class="container">@include('common_status')</div>
+			<div class="card-body">
+				<div class="row">
+					<div class="container">
+						@include('common_status')
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
